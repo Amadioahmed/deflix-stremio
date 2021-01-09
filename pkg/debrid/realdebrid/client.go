231e86da48ad55063819c15744cff1de86f3b9c0
doingodswork <doingodswork@protonmail.com>
2021-01-09 15:21:22 +0100

    Log requests to debrid services

--- pkg/debrid/realdebrid/client.go
+++ pkg/debrid/realdebrid/client.go
@@ -370,6 +370,7 @@ func (c *Client) get(ctx context.Context, url, apiToken string) ([]byte, error) 
 	fakeVersion := strconv.Itoa(rand.Intn(10000))
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0."+fakeVersion+".149 Safari/537.36")
 
+	c.logger.Debug("Sending request to RealDebrid", zap.String("request", fmt.Sprintf("%+v", req)))
 	res, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("Couldn't send GET request: %v", err)
@@ -407,6 +408,7 @@ func (c *Client) post(ctx context.Context, url, apiToken string, data url.Values
 	fakeVersion := strconv.Itoa(rand.Intn(10000))
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0."+fakeVersion+".149 Safari/537.36")
 
+	c.logger.Debug("Sending request to RealDebrid", zap.String("request", fmt.Sprintf("%+v", req)))
 	res, err := c.httpClient.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("Couldn't send POST request: %v", err)
